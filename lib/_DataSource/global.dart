dc8149f4917a901e0eeff57583067464efcfaeaa
Recircle2000 <20191220@365.hoseo.edu>
2023-05-16 03:28:26 +0900

    MVVM 패턴 적용.

--- lib/_DataSource/global.dart
+++ lib/_DataSource/global.dart
@@ -5,4 +5,6 @@ import '../_Repository/DayWeatherRepo.dart';
 import '../_ViewModel/DayweatherViewModel.dart';
 import '../_View/MainPage.dart';
 
-String? nowlocate = '정보 불러오는 중..'; //현재 위치
+// 그냥 전역 변수 저장하는 곳.
+
+String? nowlocate = '위치 수신 중..'; //현재 위치, View_Mainpage에서 사용.
--- lib/_Repository/DayWeatherRepo.dart
+++ lib/_Repository/DayWeatherRepo.dart
@@ -1,5 +1,6 @@
 import '../_DataSource/WeatherDataSource.dart';
 import '../_Model/weatherModel.dart';
+//DataSource에 접근
 
 class DayWeatherRepository {
   final WeatherDataSource _weatherDataSource = WeatherDataSource();
