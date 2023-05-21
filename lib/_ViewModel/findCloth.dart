040bf997e580f7cea400e2ade4edb554be1d601c
Recircle2000 <20191220@365.hoseo.edu>
2023-05-22 03:02:53 +0900

    위치변경 구현완료1

--- lib/_ViewModel/findCloth.dart
+++ lib/_ViewModel/findCloth.dart
@@ -2,3 +2,26 @@ import 'package:flutter/material.dart';
 import '../_Model/weatherModel.dart';
 import '../_Repository/DayWeatherRepo.dart';
 import 'package:wear_today/_Model/global.dart';
+import 'findCategory.dart';
+
+String season(int time, List<DayWeather> weatherList){
+  int temp = findTMP(time, weatherList);
+
+  bool freezing = temp < 4;
+  bool cold = temp <=5 && temp < 9;
+  bool cool = temp <=9 && temp < 12;
+  bool mild = temp <=12 && temp < 17;
+  bool warm = temp <=17 && temp < 20;
+  bool hot = temp <=20 && temp < 23;
+  bool veryHot = temp <=23 && temp < 28;
+  bool exHot = temp >= 28;
+
+
+
+  return "null";
+}
+
+ List<Map<String,String>> listup_clothes_main(int time, List<DayWeather> weatherList,int num){
+
+   return null;
+ }
